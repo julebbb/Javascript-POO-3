9ba9aaafa15e10dd99a6a2eb987a899311c06dfa
Justine Lebbrecht <justinelebbrecht@hotmail.fr>
2018-11-05 09:40:37 +0100

    change code for make element together

--- js/main.js
+++ js/main.js
@@ -3,48 +3,47 @@ var main = document.getElementById('main');
 var send = form[0][3];
 
 send.addEventListener("click",function() {
-  var title = form[0][0].value;
-  var catchphrase = form[0][1].value;
-  var descript = form[0][2].value;
+  let title = form[0][0].value;
+  let catchphrase = form[0][1].value;
+  let descript = form[0][2].value;
 
-  newProduct = new Product(title, catchphrase, descript);
-  cardDone = new Form(newProduct);
-  
-  cardDone.createcard();
-
-  form[0][0].value = "";
-  form[0][1].value = "";
-  form[0][2].value = "";
+  let cardDone = new Product(title, catchphrase, descript);
+  cardDone.createcard();  
 });
 
 
 function Product(title, catchphrase, descript) {
-  this.title = title;
-  this.catchphrase = catchphrase;
-  this.descript = descript;
-}
+  this.title = title,
+  this.catchphrase = catchphrase,
+  this.descript = descript,
 
-function Form(newProduct) {
+  this.createElement = {
+    card: document.createElement("div"),
+    titleCard: document.createElement('h2'),
+    catchphraseCard: document.createElement('h3'),
+    descriptCard: document.createElement('p')
+  };
+  this.createNode = {
+    nodeTitle: document.createTextNode(this.title),
+    nodeCatchphrase: document.createTextNode(this.catchphrase),
+    nodeDescript: document.createTextNode(this.descript)
+  };
+
   this.createcard = function() {
-    var card = document.createElement("div");
-    var titleCard = document.createElement('h2');
-    var catchphraseCard = document.createElement('h3');
-    var descriptCard = document.createElement('p');
+    this.createElement.card.className = "product";
 
-    var nodeTitle = document.createTextNode(newProduct.title);
-    var nodeCatchphrase = document.createTextNode(newProduct.catchphrase);
-    var nodeDescript = document.createTextNode(newProduct.descript);
+    this.createElement.card.appendChild(this.createElement.titleCard);
+    this.createElement.card.appendChild(this.createElement.catchphraseCard);
+    this.createElement.card.appendChild(this.createElement.descriptCard);
+    this.createElement.titleCard.appendChild(this.createNode.nodeTitle);
+    this.createElement.catchphraseCard.appendChild(this.createNode.nodeCatchphrase);
+    this.createElement.descriptCard.appendChild(this.createNode.nodeDescript);
 
-    card.className = "product";
-
-    card.appendChild(titleCard);
-    card.appendChild(catchphraseCard);
-    card.appendChild(descriptCard);
-    titleCard.appendChild(nodeTitle);
-    catchphraseCard.appendChild(nodeCatchphrase);
-    descriptCard.appendChild(nodeDescript);
-
-    main.appendChild(card);
+    main.appendChild(this.createElement.card);
+    
+    form[0][0].value = "";
+    form[0][1].value = "";
+    form[0][2].value = "";
   };
 
 }
